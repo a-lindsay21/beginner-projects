da2abc26eed44cbafa4b7b68cdf073483bf25ddd
a-lindsay21 <169114262+a-lindsay21@users.noreply.github.com>
2024-05-06 22:38:48 -0400

    MadLibs Project.py

--- MadLibs Project.py
+++ MadLibs Project.py
@@ -17,7 +17,7 @@ print('')
 print('Enjoy your story!')
 print('')
 print('')
-story1 =f"Once upon a time, in a magical forest filled with sparkling and {adjective} trees, lived a tiny {noun} 'named {name}. {name} wasnt like other {noun}s. They had a tail that was as fluffy as a {noun2} and loved to wear a tiny hat made of {noun3} ."
+story1 =f"Once upon a time, in a magical forest filled with sparkling and {adjective} trees, lived a tiny {noun} named {name}. {name} wasnt like other {noun}s. They had a tail that was as fluffy as a {noun2} and loved to wear a tiny hat made of {noun3}."
 story2 =f"One sunny morning, while skipping through a field of {noun3} flowers, a rare and special kind, {name} spotted a peculiar sight. A baby {noun2} was perched on a giant {noun4}, crying its tiny, beady eyes out. They were a strange color, {name} noticed, a sort of {color}. {name} liked them very much." 
 story3 = f"Without a moment's hesitation, {name} climbed a nearby tree using their {body_part} and gently scooped the baby into their arms. To {name}'s surprise, it was actually a {animal}!"
 story4 = f"'Don't worry little one,' said {name}, 'I'll look after you'. And so began {name}'s adventure with the baby {animal} to reunite it with its family!"
@@ -31,5 +31,5 @@ print(story4)
 print('')
 print('The End!')
 print('')
-print('I hope you had fun (or at least ended more confused than when you started, haha)! This game was coded by me, Antanya Lindsay! Find me on github @a-lindsay21!')
+print('I hope you had fun (or at least ended more confused than when you started, haha)! This game was coded by me, Antanya Lindsay! Find me on GitHub @a-lindsay21!')
 print('Thank you for playing!')
